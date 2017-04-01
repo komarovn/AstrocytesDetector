12750001e5ff30d2f73f32d0b6ea46bb547996f2
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2017-04-01 20:29:05 +0300

    Add license

--- src/main/java/com/astrocytes/client/dialogs/DialogFindAstrocytes.java
+++ src/main/java/com/astrocytes/client/dialogs/DialogFindAstrocytes.java
@@ -1,10 +1,22 @@
 /*
- * Astrocytes Detector
+ * Copyright (c) Lobachevsky University, 2017. All rights reserved.
  *
- * Designed by Komarov Nikolay.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal with the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the distribution.
  *
- * Copyright (c) Lobachevsky University, 2017.
- * All rights reserved.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Developed by: Komarov Nikolay.
  */
 package com.astrocytes.client.dialogs;
 
@@ -24,9 +36,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
-/**
- * Created by Nikolay Komarov on 05.03.2017.
- */
 public class DialogFindAstrocytes extends AbstractDialog {
     private App owner;
     private PreviewGraphicalWidget preview;
