a6f0d02c54a861b9bbf4da431c9fa4c536c7e99a
komarovn <komarov.nikolay.russia@gmail.com>
2018-05-12 13:04:07 +0300

    #27 Revert changes in disclaimer.

--- src/main/java/com/astrocytes/application/dialogs/DialogCannyEdgeDetection.java
+++ src/main/java/com/astrocytes/application/dialogs/DialogCannyEdgeDetection.java
@@ -1,10 +1,10 @@
 /*
  * Copyright (c) Lobachevsky University, 2017. All rights reserved.
  *
- * Permission isUseImage hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal with the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
- * to permit persons to whom the Software isUseImage furnished to do so, subject to the following conditions:
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
  * disclaimer.
  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
